fd7f8f726e41eef2eb79a9e212aca8dcd4bbe9a1
Alois Bake <aloisbake@gmail.com>
2025-06-03 14:10:57 +0200

    copilot

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,44 @@
-import './App.css'
-import Demo from './components/demo'
+import React, { useState } from 'react';
+import { ToggleButtonGroup, ToggleButton, Button } from '@mui/material';
 
 function App() {
-  
+  const [toggleGroups, setToggleGroups] = useState([
+    { id: 0, value: ''} // Initial group
+  ]);
+
+  const handleAddGroup = () => {
+    setToggleGroups([...toggleGroups, { id: toggleGroups.length, value: '' }]);
+  };
+
+  const handleToggleChange = (groupId, event, newValue) => {
+      const updatedGroups = toggleGroups.map(group => {
+        if (group.id === groupId) {
+          return { ...group, value: newValue };
+        }
+        return group;
+      });
+      setToggleGroups(updatedGroups);
+  };
+
   return (
-    <>
-      <Demo />
-    </>
-  )
+    <div>
+      <Button variant="contained" onClick={handleAddGroup}>
+        Add Toggle Group
+      </Button>
+      {toggleGroups.map((group) => (
+        <ToggleButtonGroup
+          key={group.id}
+          value={group.value}
+          exclusive
+          onChange={(event, newValue) => handleToggleChange(group.id, event, newValue)}
+        >
+          <ToggleButton value="left">Left</ToggleButton>
+          <ToggleButton value="center">Center</ToggleButton>
+          <ToggleButton value="right">Right</ToggleButton>
+        </ToggleButtonGroup>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
